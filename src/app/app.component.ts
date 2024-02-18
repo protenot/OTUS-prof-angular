5eefcde4420833074d76e0f318eee60507784e08
Olga_Belaya <protenot@yandex.ru>
2024-02-18 17:09:52 +0300

    add routing and formatters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { routes } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'OTUS-prof-angular';
 }
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRouting {}
---
+++ src/app/go/go.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-go',
+  standalone: true,
+  imports: [],
+  templateUrl: './go.component.html',
+  styleUrl: './go.component.css',
+})
+export class GoComponent {}
